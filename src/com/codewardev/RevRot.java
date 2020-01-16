90f6391cb868c79496f4b9c35ed75229fecfd65f
Dale Wang <dalinwanglhtz@gmail.com>
2020-01-16 22:37:54 +1100

    trying

--- src/com/codewardev/RevRot.java
+++ src/com/codewardev/RevRot.java
@@ -1,9 +1,16 @@
 package com.codewardev;
 
+import java.util.Arrays;
+
 public class RevRot {
 
 	public static String revRot(String string, int sz) {
-		// TODO Auto-generated method stub
+		String a = string.substring(0, sz);
+		int aNum = Arrays.stream(a.split("")).mapToInt(x->{
+			int i = Integer.parseInt(x);
+			return i*i*i;
+		}).sum();
+		System.out.println("aNum is "+(aNum%2));
 		return null;
 	}
 
