8980369268c01f499d13bc08ac70711e38966ec9
Dale Wang <dalinwanglhtz@gmail.com>
2020-01-18 14:01:46 +1100

    first working cut

--- src/com/codewardev/RevRot.java
+++ src/com/codewardev/RevRot.java
@@ -1,17 +1,46 @@
 package com.codewardev;
 
+// https://www.codewars.com/kata/56b5afb4ed1f6d5fb0000991/train/java
+
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class RevRot {
 
 	public static String revRot(String string, int sz) {
-		String a = string.substring(0, sz);
-		int aNum = Arrays.stream(a.split("")).mapToInt(x->{
-			int i = Integer.parseInt(x);
-			return i*i*i;
-		}).sum();
-		System.out.println("aNum is "+(aNum%2));
-		return null;
+		System.out.println("String: "+string+" size: "+string.length()+" sz: "+sz);
+		if(sz <= 0 || string.isEmpty() || sz > string.length()) return "";
+		
+		StringBuilder result = new StringBuilder();
+		for(int i=0; i<string.length(); i+=sz) {
+			if(i+sz <= string.length()) {
+				String a = string.substring(i, i+sz);
+				int aNum = Arrays.stream(a.split("")).mapToInt(x->{
+					int j = Integer.parseInt(x);
+					return j*j*j;
+				}).sum();
+				
+				if(aNum % 2 == 0) {
+					List<String> list = Arrays.asList(a.split(""));
+					Collections.reverse(list);
+					String newString = Arrays.stream(list.toArray()).map(String::valueOf).collect(Collectors.joining());
+					System.out.println("reversed: "+newString);
+					result.append(newString);
+				} else {
+					List<String> list = Arrays.asList(a.split(""));
+					Collections.rotate(list, -1);
+					String newString = Arrays.stream(list.toArray()).map(String::valueOf).collect(Collectors.joining());
+					System.out.println("rotated: "+newString);
+					result.append(newString);
+				}
+			}
+		}
+		
+		System.out.println("Result: "+result.toString());
+		
+		return result.toString();
 	}
 
 }
--- tests/com/codewardev/test/RevRotTest.java
+++ tests/com/codewardev/test/RevRotTest.java
@@ -14,9 +14,9 @@ class RevRotTest {
     @Test
     public void test1() {
         System.out.println("Fixed Tests: revRot");   
-      //  testing(RevRot.revRot("1234", 0), "");
-       // testing(RevRot.revRot("", 0), "");
-      //  testing(RevRot.revRot("1234", 5), "");
+        testing(RevRot.revRot("1234", 0), "");
+        testing(RevRot.revRot("", 0), "");
+        testing(RevRot.revRot("1234", 5), "");
         String s = "733049910872815764";
         testing(RevRot.revRot(s, 5), "330479108928157");
     }
